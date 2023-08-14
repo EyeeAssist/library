5aca8472e22594c5d469cfc69cce96d034787553
XantX <hp_xantx@hotmail.com>
2023-08-13 22:57:23 -0500

    [feature] Actualizacion de las importaciones en el proyecto

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-import { Filter } from './filter/filter.js';
-import { Zoom } from './zoom/zoom.js';
+import { Filter } from './filter/filter';
+import { Zoom } from './zoom/zoom';
 const ZoomObject = new Zoom(true);
 const FilterObject = new Filter(true);
 document.addEventListener("keydown", (event) => {

552443e03a01bcf59021e0a24af68525aae7b9a3
XantX <hp_xantx@hotmail.com>
2023-11-18 05:29:16 -0500

    Update backend link

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { Eyeeassist } from "./main/eyeeassist"
 
-const eyeeassist: Eyeeassist = new Eyeeassist(true, true, true, 'akljnv13bvi2vfo0b0bw')
+const eyeeassist: Eyeeassist = new Eyeeassist(true, true, true, 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzYmQ5MTI3Yy0xYjkyLTExZWUtYmU1Ni0wMjQyYWMxMDAwMDIiLCJpYXQiOjE3MDAzMDIwNjR9.OSQCBUfpM47vap5tVZrry6EJQMgkJdBCQ5ruBXuC-Z8')
 
 eyeeassist.start()
